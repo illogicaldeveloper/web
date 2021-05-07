dbc972a0ccd47655ff4a81be616ec99fadbac544
Ronald Michaud III <82068985+illogicaldeveloper@users.noreply.github.com>
2021-05-07 16:52:00 -0400

    Add files via upload

---
+++ quickbase.js
@@ -0,0 +1,13 @@
+
+function obj(id) {
+    return document.getElementById(id);
+};
+function html(id,data) {
+    document.getElementById(id).innerHTML = data;
+};
+function appendhtml(id,data) {
+    document.getElementById(id).innerHTML += da100ta;
+};
+function random(min,max) {
+    return Math.floor(Math.random() * max) + min  
+};
