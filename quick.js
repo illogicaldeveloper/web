59cb31a951736538cd8dbf752dfba9d2ece0b384
Ronald Michaud III <82068985+illogicaldeveloper@users.noreply.github.com>
2021-08-01 14:15:53 -0400

    Add files via upload

---
+++ quick.js
@@ -0,0 +1,33 @@
+
+var form = function(params) {    
+    // file:(url), post:(params), then:(afterFunction)
+    
+    var doneFunction = function() {
+        params.then(this.responseText);
+    }
+    
+    var http = new XMLHttpRequest();    
+    http.addEventListener("load", doneFunction);
+    http.open("POST", params.file);
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    
+    var prms = ""; for (const prop in params.post) {
+        prms += prop + "=" + params.post[prop] + "&";
+    }
+
+    http.send(prms);
+}
+
+var attach = function(element,eventlistens) {
+    for (const prop in eventlistens) {
+        element.addEventListener(prop, eventlistens[prop]);
+    }
+}
+
+var obj = function(id) { 
+    return document.getElementById(id); 
+}
+
+var html = function(id,htmlString) { 
+    document.getElementById(id).innerHTML= htmlString;
+}
